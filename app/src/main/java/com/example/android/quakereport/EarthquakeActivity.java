b59e99a4472c8048e6acc3bfba7b6d2150e073e2
anne6 <anne.chatin@gmail.com>
2019-04-18 12:42:23 -0700

    Add a check on NETWORK CONNECTIVITY

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -16,8 +16,11 @@
 package com.example.android.quakereport;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -82,13 +85,29 @@ public class EarthquakeActivity extends AppCompatActivity
             }
         });
 
-        // Get a reference to the LoaderManager, in order to interact with loaders.
-        LoaderManager loaderManager = getLoaderManager();
 
-        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-        // because this activity implements the LoaderCallbacks interface).
-        loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null)
+        {
+            NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+
+            if (activeNetwork != null && activeNetwork.isConnected()){
+                // Get a reference to the LoaderManager, in order to interact with loaders.
+                LoaderManager loaderManager = getLoaderManager();
+
+                // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+                // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+                // because this activity implements the LoaderCallbacks interface).
+                loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+            } else {
+                ((TextView) findViewById(R.id.emptyListTextView)).setText(R.string.no_Internet_Connectivity);
+                // Hide the loading indicator
+                (findViewById(R.id.loading_indicator)).setVisibility(View.GONE);
+            }
+        }
+
+
     }
 
 
